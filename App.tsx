9cd2a22ed0d4be3faa88b52b6af6dae5f3c2d33a
Ed <75823130+edixa@users.noreply.github.com>
2021-11-15 20:20:08 -0600

    creé la app de twitter y muestra pantallas

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('FeedScreen')}
+      />
+    </View>
+  );
+}
+const FeedScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Inicio' }} />
+        <Stack.Screen name="FeedScreen" component={FeedScreen}  />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
